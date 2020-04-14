9e1213adb75762e7dfd027ce66ea6d688e36f7c6
QuangLa <quang.la.gx@renesas.com>
2020-04-14 17:14:42 +0700

    update

--- pcl_worksheet_handler.py
+++ pcl_worksheet_handler.py
@@ -2,9 +2,6 @@ from threading import Thread
 # Define number of Thread
 NUMOFTHREAD = 4
 
-# Global hold worksheet information
-g_ws_rows         = None
-g_ws_merged_cells = None
 # Global hold search worksheet Item
 # Length of Target must be equal with NUMOFTHREAD
 g_target = [
@@ -15,29 +12,33 @@ g_target = [
 ]
 # Gloabl hold the return value of Thread
 g_thread_return = [None] * NUMOFTHREAD
+g_test_case_id = []
 
 # Thread function
-def ws_thread_seek(i_ws_rows, i_string, i_index, i_matchcase=False):
+def ws_thread_seek(i_ws_rows, i_ws_merged_cell, i_target, i_index, i_matchcase=False):
     global g_thread_return
 
+    if (i_target)
     for loop_rows in i_ws_rows:
         for loop_cell in loop_rows:
-            if ((loop_cell.value == i_string) if i_matchcase else (loop_cell.value in i_string)):
-                g_thread_return[i_index] = loop_cell.coordinate
-                return
+            if ((loop_cell.value == i_target) if i_matchcase else (loop_cell.value in i_target)):
+                print(i_target, loop_cell.coordinate)
+                for each_merged_cells in i_ws_merged_cell:
+                    if (loop_cell.coordinate in each_merged_cells):
+                        g_thread_return[i_index] = each_merged_cells.bounds
+                        return
+                g_thread_return[i_index] = (loop_cell.column, loop_cell.row, loop_cell.column, loop_cell.row)
+
+def pcl_shift_left(i_ws_rows):
+    pass
 
 def pcl_worksheet_init(ws):
-    global g_ws_rows
-    global g_ws_merged_cells
     global g_target
     global g_thread_return
 
-    g_ws_merged_cells = ws.merged_cells
-
     l_thread = [None] * NUMOFTHREAD
     for loop_i in range(NUMOFTHREAD):
-        print(loop_i)
-        l_thread[loop_i] = Thread(target=ws_thread_seek, args=(ws.rows, g_target[loop_i], loop_i, True))
+        l_thread[loop_i] = Thread(target=ws_thread_seek, args=(ws.rows, ws.merged_cells, g_target[loop_i], loop_i, True))
         l_thread[loop_i].start()
 
     for loop_i in range(NUMOFTHREAD):
@@ -45,4 +46,4 @@ def pcl_worksheet_init(ws):
 
     print(g_thread_return)
 
-    
+
---
+++ pcl_ws.py
@@ -0,0 +1,16 @@
+class cell_info(object):
+    __slots__=['value']
+    def __init__(i_ws, i_col, i_row, i_coor):
+        self.value = value
+        self.coods = 
+        self.coordinate = i_coordinate
+        self.merged_cell = i_merged_cell
+
+ws_row = None
+
+def ws_find(i_ws, i_target, i_match_case=False):
+    if (isinstance(i_ws, str)):
+
+
+def pcl_ws_init(ws):
+
